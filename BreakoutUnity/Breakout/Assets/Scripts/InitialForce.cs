2403680989f2a8f88a0ebed84b29b14303299826
Krzysztof Perwel <perkristo@gmail.com>
2015-12-15 21:00:51 +0100

    Initial commit, basic structure

---
+++ BreakoutUnity/Breakout/Assets/Scripts/InitialForce.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class InitialForce : MonoBehaviour {
+    private Rigidbody2D rigidbodyComponent;
+    public bool randomStart = true;
+
+    public Vector2 direction = new Vector2(1f, 1f);
+    public float forceValue = 10f;
+
+    void Start () {
+
+        rigidbodyComponent = this.GetComponent<Rigidbody2D>();
+        rigidbodyComponent.AddTorque(Random.Range(-3f, 3f));
+        Vector2 force = Vector2.zero;
+
+        if (this.randomStart)
+        {
+            force = new Vector2(Random.Range(-10f, 10f), Random.Range(-1f, 1f)).normalized * this.forceValue;
+        } else
+        {
+            force = this.direction.normalized * this.forceValue;
+        }
+        rigidbodyComponent.AddForce(force);
+    }
+}
