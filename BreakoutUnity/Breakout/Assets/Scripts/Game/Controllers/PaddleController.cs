f4df2868cfb883caabed60cb0118b9a253760754
Sebastian Koscielny <seb.koscielny@gmail.com>
2015-12-16 23:16:48 +0100

    initial GameScene

---
+++ BreakoutUnity/Breakout/Assets/Scripts/Game/Controllers/PaddleController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	public PaddleModel paddleModel;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject objectHit=null;
+		this.handleTouchOnUpdate(objectHit);
+		this.handleMouseOnUpdate(objectHit);
+
+	}
+
+	void handleTouchOnUpdate(GameObject objectHit){
+		
+		foreach(Touch touch in Input.touches){
+			if(touch.phase == TouchPhase.Began){
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit)){
+					if(hit.collider.gameObject.Equals(paddleModel)){
+						objectHit = hit.collider.gameObject;
+					}
+				}
+			}else if(touch.phase == TouchPhase.Moved){
+				if(objectHit){
+					objectHit.transform.position = new Vector3(touch.position.x,touch.position.y,0);
+				}
+			}else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled){
+				objectHit = null;
+			}
+		}
+	}
+	void handleMouseOnUpdate(GameObject objectHit){
+		if(Input.GetMouseButton(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				if(paddleModel.canMove){
+					paddleModel.transform.position = new Vector2(hit.point.x, paddleModel.gameObject.transform.position.y);
+				}
+			}
+		}
+	}
+
+}
---
+++ BreakoutUnity/Breakout/Assets/Scripts/Game/Models/PaddleModel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleModel : MonoBehaviour {
+
+	public bool canMove = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(){
+		Debug.Log("HUJ");
+		canMove = false;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("Cipa");
+		canMove = false;
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log (other);
+		canMove = true;
+	}
+}
