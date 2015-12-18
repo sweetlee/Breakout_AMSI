981f38a544db45815f778dc7491af749d40f91a5
Sebastian Koscielny <seb.koscielny@gmail.com>
2015-12-18 16:56:18 +0100

    initial gameplay, initial physic, initial movement (keyboard).

---
+++ BreakoutUnity/Breakout/Assets/Scripts/Game/Controllers/BallController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController: MonoBehaviour {
+
+	public float ballInitialVelocity = 600f;
+
+
+	private Rigidbody rb;
+	private bool ballInPlay = false;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			Debug.Log("HUJ");
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+		}
+	}
+}
--- BreakoutUnity/Breakout/Assets/Scripts/Game/Controllers/PaddleController.cs
+++ BreakoutUnity/Breakout/Assets/Scripts/Game/Controllers/PaddleController.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class PaddleController : MonoBehaviour {
 
-	public PaddleModel paddleModel;
-	
+	public float paddleSpeed = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,42 +12,10 @@ public class PaddleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GameObject objectHit=null;
-		this.handleTouchOnUpdate(objectHit);
-		this.handleMouseOnUpdate(objectHit);
-
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		Vector3 playerPos = new Vector3 (Mathf.Clamp (xPos, -13.0f, 13.0f), transform.position.y, 0f);
+		transform.position = playerPos;
 	}
 
-	void handleTouchOnUpdate(GameObject objectHit){
-		
-		foreach(Touch touch in Input.touches){
-			if(touch.phase == TouchPhase.Began){
-				Ray ray = Camera.main.ScreenPointToRay(touch.position);
-				RaycastHit hit;
-				if(Physics.Raycast(ray, out hit)){
-					if(hit.collider.gameObject.Equals(paddleModel)){
-						objectHit = hit.collider.gameObject;
-					}
-				}
-			}else if(touch.phase == TouchPhase.Moved){
-				if(objectHit){
-					objectHit.transform.position = new Vector3(touch.position.x,touch.position.y,0);
-				}
-			}else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled){
-				objectHit = null;
-			}
-		}
-	}
-	void handleMouseOnUpdate(GameObject objectHit){
-		if(Input.GetMouseButton(0)){
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit)){
-				if(paddleModel.canMove){
-					paddleModel.transform.position = new Vector2(hit.point.x, paddleModel.gameObject.transform.position.y);
-				}
-			}
-		}
-	}
 
 }
